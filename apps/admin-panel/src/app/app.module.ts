a4fc71ed87953d27659849710bf188b2f8e92967
jiteshDigixito <jitesh.suyal@digixito.com>
2025-06-11 15:45:27 +0530

    Added flisght section in admin, updated hotel entity in repo

--- apps/admin-panel/src/app/app.module.ts
+++ apps/admin-panel/src/app/app.module.ts
@@ -4,14 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommissionModule } from './commission/commission.module';
 import { HomeModule } from './home/home.module';
-import {HotelierModule} from '../hotelier/hotelier.module';
-import {PackageModule} from '../package/package.module';
+import { HotelierModule } from '../hotelier/hotelier.module';
+import { PackageModule } from '../package/package.module';
 import { CustomerModule } from '../customer/customer.module';
+import { FlightsModule } from '../flights/flights.module';
 
 @Module({
-    imports: [CommissionModule,HomeModule,HotelierModule,PackageModule,CustomerModule ],
+    imports: [CommissionModule, HomeModule, HotelierModule, PackageModule, CustomerModule, FlightsModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {} 
+export class AppModule { }
 
